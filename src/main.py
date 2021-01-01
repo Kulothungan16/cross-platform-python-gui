10a1f9c79a7c17eabb6f9a9ca9fa25f4fcd83c5d
Kulothungan U.G <kulothunganug@gmail.com>
2021-01-01 11:19:58 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,22 +1,49 @@
 # this is needed for supporting Windows 10 with OpenGL < v2.0
 # Example: VirtualBox w/ OpenGL v1.1
-import platform, os
-if platform.system() == 'Windows':
+from kivy.lang import Builder
+from kivy.utils import platform
+from kivymd.app import MDApp
+
+from camy import Camy
+
+if platform == 'win32':
     os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 
-import kivy
-#kivy.require('1.0.6') # replace with your current kivy version !
 
-from kivy.app import App
-from kivy.uix.label import Label
+KV = """
+BoxLayout:
+    Button:
+        text: 'Open'
+        on_release: app.open()
+    MDLabel:
+        id: lbl
+        halign: 'center'
+"""
 
-class MyApp(App):
 
+class MainApp(MDApp):
     def build(self):
-        return Label(text='Hello world!')
+        self.root = Builder.load_string(KV)
+
+    def on_pic(self, *args):
+        try:
+            self.root.ids.lbl.text = str(args)
+        except Exception as e:
+            self.root.ids.lbl.text = str(e)
+
+    def on_start(self):
+        try:
+            self.cam = Camy()
+            self.cam.bind(on_picture_taken=self.on_pic)
+        except Exception as e:
+            self.root.ids.lbl.text = str(e)
+
+    def open(self):
+        try:
+            self.cam.open()
+        except Exception as e:
+            self.root.ids.lbl.text = str(e)
 
 
-if __name__ == '__main__':
-    MyApp().run()
-
+MainApp().run()
 
